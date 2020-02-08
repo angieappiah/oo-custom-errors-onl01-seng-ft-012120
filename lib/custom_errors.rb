9568ea00c3bb73a57f9bff5825384145a1bc7b93
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-08 08:06:27 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,11 +8,10 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
-      
+      begin
       raise PartnerError
-    else
-      person.partner = self
-    end
+      rescue
+    
   end
  
  class PartnerError < StandardError
